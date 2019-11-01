fbe1716d21b4f28de53b71f88b726663275f020a
Cosmin Popescu <cosminp9@gmail.com>
2019-11-01 16:04:22 +0200

    iCase content view fixtures

--- src/components/Case.js
+++ src/components/Case.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react'
+import { Switch } from '@blueprintjs/core'
 
 class Case extends Component {
+  constructor() {
+    super()
+
+    this.handlePublicChange = this.handlePublicChange.bind(this)
+    this.state = { safeAreaView: false }
+  }
+
+  handlePublicChange() { this.setState({ safeAreaView: !this.state.safeAreaView }) }
+
   render() {
+    const _switch = (
+      <Switch checked={this.state.safeAreaView} label="SafeAreaView" onChange={this.handlePublicChange} />
+    )
+
     const iCase = (
       <div className='phone'>
         <div className='notch'><div className='notch-top-left-corner'></div><div className='notch-top-right-corner'></div></div>
-        <div className='view safe-area'>
+        <div className={this.state.safeAreaView ? 'view safe-area' : 'view'}>
           <div className='scroll-view'>
             <div className='content'>
+              <h2 className='ff-r'>Content</h2>
             </div>
           </div>
         </div>
         <div className='button'></div>
       </div>
     )
-    return iCase
+
+    return (
+      <>
+        {_switch}
+        {iCase}
+      </>
+    )
   }
 }
 
