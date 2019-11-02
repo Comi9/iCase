1979ffe9d9b9b5675445c72eb15626cf6f7aafbf
Cosmin Popescu <cosminp9@gmail.com>
2019-11-02 09:34:46 +0200

    LocalStorage and component state two ways sync

--- src/components/Case.js
+++ src/components/Case.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Button, Switch } from '@blueprintjs/core'
+import _ from 'lodash'
 
-const DEFAULT_LOCAL_KEYS = {
+const ICASE_DEFAULT_LOCAL_KEYS = {
   safeAreaView: false
 }
 
@@ -10,17 +11,33 @@ class Case extends Component {
     super()
 
     this.handleSafeAreaViewChange = this.handleSafeAreaViewChange.bind(this)
-    this.state = { ...DEFAULT_LOCAL_KEYS }
+    this.handleLocalStorageReset = this.handleLocalStorageReset.bind(this)
+    this.state = { ...ICASE_DEFAULT_LOCAL_KEYS }
   }
 
-  componentDidUpdate(prevProps, prevState) { localStorage.state = JSON.stringify(this.state) }
+  componentDidMount() {
+    if (_.isEmpty(localStorage.ICASE_LOCAL_KEYS)) {
+      localStorage.ICASE_LOCAL_KEYS = JSON.stringify(this.state)
+    } else {
+      this.setState({ ...JSON.parse(localStorage.ICASE_LOCAL_KEYS) })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (_.differenceWith(this.state, prevState, _.isEqual)) { localStorage.ICASE_LOCAL_KEYS = JSON.stringify(this.state) }
+  }
 
   handleSafeAreaViewChange() { this.setState({ safeAreaView: !this.state.safeAreaView }) }
+
+  handleLocalStorageReset() {
+    localStorage.ICASE_LOCAL_KEYS = JSON.stringify(ICASE_DEFAULT_LOCAL_KEYS)
+    this.setState({ ...JSON.parse(localStorage.ICASE_LOCAL_KEYS) })
+  }
 
   render() {
     const _switch = (
       <>
-        <Button icon='refresh' />
+        <Button icon='refresh' onClick={this.handleLocalStorageReset} />
         <Switch checked={this.state.safeAreaView} label='SafeAreaView' onChange={this.handleSafeAreaViewChange} />
       </>
     )
@@ -35,11 +52,11 @@ class Case extends Component {
             </div>
           </div>
         </div>
-        <div className='button'></div>
-        <div className='ringtone'></div>
-        <div className='volume-up'></div>
-        <div className='volume-down'></div>
-        <div className='power'></div>
+        <div className='button' />
+        <div className='ringtone' />
+        <div className='volume-up' />
+        <div className='volume-down' />
+        <div className='power' />
       </div>
     )
 
