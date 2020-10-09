c381e514bf63e1bf9d3f1ed6c372b3bae5f4e909
Cosmin <cosminp9@gmail.com>
2020-10-09 20:37:19 +0300

    Updates

---
+++ precache-manifest.2a3ac6acf4e72457fedbac36ec775616.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a649bddf3988a1a37faf64724b1bdd4",
+    "url": "/iCase/index.html"
+  },
+  {
+    "revision": "8777989a760fc0499da1",
+    "url": "/iCase/static/css/2.59a261c3.chunk.css"
+  },
+  {
+    "revision": "186a31896bae3e6f99e7",
+    "url": "/iCase/static/css/main.78dc6f24.chunk.css"
+  },
+  {
+    "revision": "8777989a760fc0499da1",
+    "url": "/iCase/static/js/2.895d9916.chunk.js"
+  },
+  {
+    "revision": "186a31896bae3e6f99e7",
+    "url": "/iCase/static/js/main.e2ec299c.chunk.js"
+  },
+  {
+    "revision": "d225bc037f5bb841c6ca",
+    "url": "/iCase/static/js/runtime-main.526d2c6a.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/iCase/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/iCase/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/iCase/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/iCase/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/iCase/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/iCase/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
